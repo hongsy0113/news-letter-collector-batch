80bb06f2a19e34e66a501815019f35143fefdd31
hongsy0113 <hongsy0113@gmail.com>
2023-05-22 21:28:32 +0900

    :tada: 프로젝트 생성

---
+++ src/main/kotlin/com/yooni/newsletter/NewsletterApplication.kt
@@ -0,0 +1,11 @@
+package com.yooni.newsletter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NewsletterApplication
+
+fun main(args: Array<String>) {
+	runApplication<NewsletterApplication>(*args)
+}
