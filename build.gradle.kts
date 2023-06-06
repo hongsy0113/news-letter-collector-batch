3a7d3546de3e13f13ecce1b15698b820ba0fe39f
yooni <hongsy0113@gmail.com>
2023-06-06 19:54:00 +0900

    :wrench: gitignore 적용되도록 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,25 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-batch")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.apache.httpcomponents:httpclient:4.5.10")
+
+	// loggin
+	implementation("io.github.microutils:kotlin-logging:1.7.4")
+	implementation("com.getsentry.raven:raven-logback:8.0.3")
+	implementation("io.sentry:sentry-spring-boot-starter:5.6.1")
+	implementation("io.sentry:sentry-logback:5.6.1")
+
+
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.batch:spring-batch-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
+	testImplementation("io.kotest:kotest-runner-junit5:5.3.2")
+	testImplementation("io.kotest:kotest-assertions-core:5.3.2")
 }
 
 tasks.withType<KotlinCompile> {
